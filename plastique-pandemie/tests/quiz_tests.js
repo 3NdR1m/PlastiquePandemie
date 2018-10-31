c4b64167b29069535ed52d2c75f854ed5d2dd625
Diabel Elian <diabelelian@gmail.com>
2018-10-31 10:26:37 -0400

    ajouter des tests

--- plastique-pandemie/tests/quiz_tests.js
+++ plastique-pandemie/tests/quiz_tests.js
@@ -5,8 +5,41 @@ QUnit.test( "",
 });
 
 //ajouterPoint
+QUnit.test( "WHEN_pointScored_THEN_ajouterPoint_adds1", 
+    function( assert ) {
+        const score = 2;
+        const expectedScore = 3;
+
+        assert.ok( ajouterPoint(score) == expectedScore );
+});
+QUnit.test( "WHEN_pointScored_THEN_ajouterPoint_adds1", 
+    function( assert ) {
+        const score = 4;
+        const expectedScore = 5;
+
+        assert.ok( ajouterPoint(score) == expectedScore );
+});
+QUnit.test( "WHEN_pointScored_THEN_ajouterPoint_adds1", 
+    function( assert ) {
+        const score = 0;
+        const expectedScore = 1;
+
+        assert.ok( ajouterPoint(score) == expectedScore );
+});
+
 //obtenirPoint
+QUnit.test( "WHEN_prompted_THEN_obtenirPoint_RETURNS_totalPointage", 
+    function( assert ) {
+        assert.ok( obtenirPoint() == totalpointage );
+});
+
 //estFinPartie
+QUnit.test( "WHEN_gameOver_THEN_estFinPartie_RETURNS_true", 
+    function( assert ) {
+        questionCourante = 3;
+        assert.ok( estFinPartie(questionCourante));
+});
+
 //chargerQuestionSuivante
 //obtenirBonneReponse
 //obtenirChoix
