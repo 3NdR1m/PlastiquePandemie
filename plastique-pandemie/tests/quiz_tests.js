0023cfc338278cb9bcc03be82089f7ba32f0f697
Diabel Elian <diabelelian@gmail.com>
2018-10-31 10:02:41 -0400

    Initiallized Test

---
+++ plastique-pandemie/tests/quiz_tests.js
@@ -0,0 +1,28 @@
+//validerQuestion
+QUnit.test( "", 
+    function( assert ) {
+        assert.ok();
+});
+
+//ajouterPoint
+//obtenirPoint
+//estFinPartie
+//chargerQuestionSuivante
+//obtenirBonneReponse
+//obtenirChoix
+//afficherBonneReponse
+//majPointage
+//majTotalQuestion
+//majTexteChoix
+//majTexteQuestion
+//majNoQuestionCourant
+//remiseAZeroBoutons
+//majProgression
+//majInterface
+//selectionnerChoix
+//afficherBoiteFinDeJeu
+
+QUnit.test( "", 
+    function( assert ) {
+        assert.ok();
+});
