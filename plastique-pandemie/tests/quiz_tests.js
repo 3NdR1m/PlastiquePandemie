504d2579973648b4122fbd08a61be39cf2bcf3ec
Diabel Elian <diabelelian@gmail.com>
2018-10-31 11:32:39 -0400

    Tests work again

--- plastique-pandemie/tests/quiz_tests.js
+++ plastique-pandemie/tests/quiz_tests.js
@@ -1,37 +1,38 @@
+/** 
+ * 
+ * 
+ */
 //validerQuestion
-QUnit.test( "", 
-    function( assert ) {
-        assert.ok();
-});
 
 //ajouterPoint
 QUnit.test( "WHEN_pointScored_THEN_ajouterPoint_adds1", 
     function( assert ) {
-        const score = 2;
-        const expectedScore = 3;
-
-        assert.ok( ajouterPoint(score) == expectedScore );
+        totalPointage = 2;
+        var expectedScore = totalPointage + 1;
+        ajouterPoint()
+        assert.ok( expectedScore == totalPointage )
 });
 QUnit.test( "WHEN_pointScored_THEN_ajouterPoint_adds1", 
     function( assert ) {
-        const score = 4;
-        const expectedScore = 5;
-
-        assert.ok( ajouterPoint(score) == expectedScore );
+        totalPointage = 4;
+        var expectedScore = totalPointage + 1;
+        ajouterPoint()
+        assert.ok( expectedScore == totalPointage )
 });
 QUnit.test( "WHEN_pointScored_THEN_ajouterPoint_adds1", 
     function( assert ) {
-        const score = 0;
-        const expectedScore = 1;
-
-        assert.ok( ajouterPoint(score) == expectedScore );
+        totalPointage = 0;
+        var expectedScore = totalPointage + 1;
+        ajouterPoint()
+        assert.ok( expectedScore == totalPointage )
 });
 
 
 //obtenirPoint
 QUnit.test( "WHEN_prompted_THEN_obtenirPoint_RETURNS_totalPointage", 
     function( assert ) {
-        assert.ok( obtenirPoint() == totalpointage );
+        var totalpointage = 0;
+        assert.ok( obtenirPointage() == totalpointage );
 });
 
 //estFinPartie
